01fa156b9983084d482008f22c9b07e8b8684030
Rahul Mandal <rahul1810mandal@gmail.com>
2024-03-29 15:18:32 +0530

    update

---
+++ frontend/npm/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+<aside
+className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r 
+bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 hover:bg-gray-600/10 border-gray-800 text-white'>
+TEST SIDE BAR
+</aside>
+  )
+};
+
+export default Sidebar;
